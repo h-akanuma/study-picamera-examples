1c62c006fc9524f84d1230582d7e164f59a1e94a
Hiroaki Akanuma <hiroaki.akanuma@gmail.com>
2018-10-24 19:31:17 +0900

    Change rectangle color

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -70,7 +70,7 @@ class PersonDetector(object):
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype('int')
             label = '{}: {:.2f}%'.format('Person', confidence * 100)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
             y = startY - 15 if startY - 15 > 15 else startY + 15
             cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
             count += 1
